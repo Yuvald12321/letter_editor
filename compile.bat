84765bfc6c490ed9aceab7aa916d8690f15bb240
Yuval <yuvalivshits@gmail.com>
2026-06-16 19:46:40 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,2 @@
-pyinstaller --onefile --noconsole --icon "logo.ico" -n "The letter editor" main.py
+pyinstaller --onefile --noconsole --noconfirm --icon "logo.ico" -n "The letter editor" main.py
 rd /S /Q "build"
-del /Q "The letter editor.spec"
