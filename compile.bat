589701e1a4fc6ccc5d0c2d476ccf420887c72619
Yuval <yuvalivshits@gmail.com>
2026-06-18 18:46:03 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-call ".venv\Scripts\pyinstaller.exe" --specpath "build" --onefile --noconsole --icon "logo.ico" -n "The letter editor" main.py
+call ".venv\Scripts\pyinstaller.exe" --onefile --noconsole --icon "logo.ico" -n "The letter editor" main.py
 rd /S /Q "build"
